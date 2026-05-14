143d64c449a7a1c766ec1da5e529a12b12ea3582
Ling Yu <echo.aimless@gmail.com>
2026-05-14 14:24:41 +0800

    feat: setup unocss presets

---
+++ uno.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, presetAttributify, presetWind4 } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetWind4({
+      preflights: {
+        reset: true,
+      },
+    }),
+    presetAttributify(),
+  ],
+})
